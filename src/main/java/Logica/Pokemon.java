d30342ec331071ddbdd186ccf9751f5bfa6babf5
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-07-14 20:00:00 -0300

    Creo las clases abstractas

--- src/main/java/Logica/Pokemon.java
+++ src/main/java/Logica/Pokemon.java
@@ -1,6 +1,14 @@
 
 package Logica;
 
-public class Pokemon {
+public abstract class Pokemon {
+    protected int numPokedex;
+    protected String nombre;
+    protected double peso;
+    protected String sexo;
+    protected int temporada;
     
+    protected abstract void atacarPlacaje();
+    protected abstract void atacarAraniazo();
+    protected abstract void atacarMordisco();
 }
