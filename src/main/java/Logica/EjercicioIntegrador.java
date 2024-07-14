868c79159ab3bf4552cf548cc25c9109e1189eea
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-07-14 19:37:28 -0300

    Creo el proyecto integrador de java: proyecto pokemon, usamos clases abstractas e interfaces

---
+++ src/main/java/Logica/EjercicioIntegrador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Logica;
+
+/**
+ *
+ * @author salu
+ */
+public class EjercicioIntegrador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
